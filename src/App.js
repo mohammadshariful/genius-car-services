7b96d636bbbc0034f2b28c81102f55b22fb34834
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-04-10 23:31:24 +0600

    services section added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home/Home";
+import Footer from "./pages/Shared/Footer/Footer";
+import Header from "./pages/Shared/Header/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <h2>
+        <small> &copy; copyright 2022 </small>
+      </h2>
+    </div>
+  );
+};
+
+export default Footer;
